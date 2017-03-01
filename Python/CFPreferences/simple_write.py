925a3d4006ce9492747369a8f72252143bdf9f99
Armin Briegel <ab@scriptingosx.com>
2017-03-01 14:54:02 +0100

    added profiles

--- Python/CFPreferences/simple_write.py
+++ Python/CFPreferences/simple_write.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
-from Foundation import CFPreferencesSetAppValue
-                       
-CFPreferencesSetAppValue("idleTime", 600, "com.apple.screenSaver")
+from Foundation import CFPreferencesSetAppValue, /
+                       CFPreferencesAppSynchronize
+         
+app_id = "com.apple.screenSaver"              
+CFPreferencesSetAppValue("idleTime", 600, app_id)
+CFPreferencesAppSynchronize(app_id)
