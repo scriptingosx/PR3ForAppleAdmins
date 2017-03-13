edd64343f87c15d7033d07bc8849127b830f3056
Armin Briegel <ab@scriptingosx.com>
2017-03-13 16:00:31 +0100

    changed sample files

--- Python/FoundationPlist_read.py
+++ Python/FoundationPlist_read.py
@@ -2,10 +2,10 @@
 
 import FoundationPlist
 
-shop_list = FoundationPlist.readPlist("../examples/list.plist")
+shop_list = FoundationPlist.readPlist("../examples/list_binary.plist")
 print shop_list
 
 print
 
-berry_dict = FoundationPlist.readPlist("../examples/berry.plist")
+berry_dict = FoundationPlist.readPlist("../examples/berry_ascii.plist")
 print berry_dict
